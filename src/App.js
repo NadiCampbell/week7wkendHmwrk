38846ac03ff388b838e7573cd9aff6bc794640ea
Nadi Campbell <nadicampbell97@hotmail.com>
2023-07-14 19:05:43 +0100

    add function to call api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import PokemonContainer from './Container/PokemonContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokémon</h1>
+      <PokemonContainer/>
+
+      {/* <select>
+        <option value="" disabled >Pick A Pokemon</option>
+        <option>Pikachu</option>
+        <option>Bulbasaur</option>
+        <option>Squirtle</option>
+        <option>Charmeleon</option>
+      </select> */}
     </div>
   );
 }
---
+++ src/Container/PokemonContainer.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from 'react'; 
+
+
+const PokemonContainer = () => {
+    
+    const [pokemon, setPokemon] = useState([]);
+    const [chosenPokemon, setChosenPokemon] = useState(null);
+    const [response, setResponse] = useState(null);
+    
+    useEffect(() => {
+        fetchPokemon();
+    }, []);
+
+    
+    const fetchPokemon = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon")
+        .then(response => response.json())
+        .then(pokemonData => setPokemon(pokemonData))
+    }
+
+    useEffect(() => {
+        if(chosenPokemon) {
+            fetchSpecificPokemon();
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [chosenPokemon]);
+
+    
+    const fetchSpecificPokemon = () => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${chosenPokemon}`)
+        .then(response => response.json())
+        .then(pokemonData => setResponse(pokemonData))
+    }
+
+
+    return (
+        <>
+       <select onChange={(e) => setChosenPokemon(e.target.value)}>
+       <option>Pick A Pokemon</option>
+        {pokemon.results?.map((x, index) => {
+            return <option key={index}>{x.name}</option>
+        })}
+       </select><br></br>
+
+      { response && "chosen:" + JSON.stringify(response.abilities)}
+       </>
+    )
+
+}
+
+export default PokemonContainer;
